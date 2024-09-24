26040bc741f2f88cf4ec7ccd58591dc5b9c617db
Søren Frederik Ploug <soren.ploug04@gmail.com>
2024-09-24 14:03:54 +0200

    Hydac opdateret

---
+++ HydacProject/Classes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydacProject
+{
+    internal class Classes
+    {
+        //dette er en ny opdatering 1
+    }
+}
---
+++ HydacProject/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading.Tasks;
+
+namespace HydacProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tjek om filstien eksistere til Hydac.txt
+            // Hvis den ikke eksistere opret en fil og filsti til Hydac.txt
+            // Hvis den eksistere, gå videre :)
+
+            // While true
+            while (true)
+            {
+                
+                // Switch
+                
+
+                // Menuvalg
+                Console.WriteLine("Velkommen til Hydac komme/gå system\n");
+
+                Console.WriteLine("Vælg/tast én af nedestående valgmuligheder: \n");
+
+                Console.WriteLine("Tast 1 for at tilføje besøgende");
+                Console.WriteLine("Tast 2 for at fjerne besøgende");
+                Console.WriteLine("Tast 3 for at se alle aktuelle besøgende");
+                Console.WriteLine("Tast 4 for at redigere besøgende");
+                Console.WriteLine("Tast 5 for historik over besøgende");
+                Console.WriteLine("Tast 6 for at lukke programmet\n");
+
+                switch(Convert.ToInt32(Console.ReadLine()))
+
+                   {
+                    case 1:
+                        {
+
+
+                            break;
+                        }
+                        case 2:
+                        {
+                            break;
+                        }
+                        case 3:
+
+                        break;
+
+                        case 4:
+
+                        break;
+
+                        case 5:
+
+                        break;
+
+                        case 6:
+
+                        break;
+
+                }
+                // Tast 1 for at tilføje besøgende
+                // tobiTheHobi laver 1 
+                //Musy laver 2
+                //Emil Laver 3
+                //søren laver 6
+
+
+                Console.WriteLine("Skriv besøgendes navn: ");
+                Console.ReadLine();
+                Console.WriteLine("Skriv besøgendes firma: ");
+                Console.ReadLine();
+
+                // Tast 2 for at fjerne besøgende
+
+
+                // Tast 3 for at se alle aktuelle besøgende
+
+
+                // Tast 4 for at redigere besøgende
+
+
+                // Tast 5 for historik over besøgende
+
+
+                // Tast 6 for at lukke programmet
+            }
+        }
+    }
+}
