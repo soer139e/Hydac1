5cb9e3a9cb54409dd35ca6aa982d2accc7c9b645
Søren Frederik Ploug <soren.ploug04@gmail.com>
2024-10-02 13:03:39 +0200

    Finished file handler for visitor

--- HydacProject/FileHandler.cs
+++ HydacProject/FileHandler.cs
@@ -14,22 +14,26 @@ namespace HydacProject
 
         //SaveVisitorToFile saves the Visitor list to a .txt file
         //It accepts a Visitorlist and a filePath
-        public static void SaveVisitorToFile(Visitor visitor, string filePath)
+        public static void SaveVisitorToFile(VisitorList visitorList, string filePath)
         {
             //Gets StreamWriter library and creates a instances of it that will write to the specified .txt file 
             using (StreamWriter writer = new StreamWriter(filePath, true))
             {
 
-                
-                    // Writes the properties in Visitor separated by a comma
-                    writer.WriteLine($"{visitor.companyName} ,{visitor.personName},{visitor.safetyBrochurGiven},{Visitor.responsableForVisitor}");
+
+                // Writes the properties in Visitor separated by a comma
+               
+                writer.WriteLine($"{visitorList.visitors[visitorList.visitorCount].companyName}," +
+                    $"{visitorList.visitors[visitorList.visitorCount].personName}," +
+                    $"{visitorList.visitors[visitorList.visitorCount].safetyBrochurGiven}," +
+                    $"{visitorList.visitors[visitorList.visitorCount].responsableForVisitor}");
 
                 
 
             }
         }
         //ReadVisitorFromFile will read a .txt file and add all instances of Visitors to the VisitorList
-        public void ReadVisitorFromFile(string filepath)
+        public void ReadVisitorFromFile(VisitorList visitorlist,string filepath)
         {
             string[] lineSplit;
             string line;
@@ -41,11 +45,15 @@ namespace HydacProject
                 //Loops through the .txt file until all is read
                 while(line != null)
                 {
+                    // Gets the current line and splits it at every ',' turning it into a string array
                     lineSplit = line.Split(new char[] { ',' });
+                    // Assigns visitor the read data
                     visitor.companyName = lineSplit[0];
                     visitor.personName = lineSplit[1];
                     visitor.safetyBrochurGiven = Convert.ToBoolean(lineSplit[2]);
                     visitor.responsableForVisitor = lineSplit[3];
+                    // adds the visitor to a visitor list
+                    visitorlist.visitors[visitorlist.visitorCount] = visitor;
 
                     line = reader.ReadLine();
                 }
